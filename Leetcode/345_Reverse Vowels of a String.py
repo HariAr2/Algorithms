f3adc8e0984f2bf1160f33948f437e26817f524a
HariAr2 <ha7014@srmist.edu.in>
2024-06-17 21:23:15 +0530

    added 345 leetcode EASY

--- Leetcode/345_Reverse Vowels of a String.py
+++ Leetcode/345_Reverse Vowels of a String.py
@@ -4,8 +4,6 @@ Given a string s, reverse only all the vowels in the string and return it.
 
 The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
 
- 
-
 Example 1:
 
 Input: s = "hello"
@@ -15,12 +13,10 @@ Example 2:
 Input: s = "leetcode"
 Output: "leotcede"
  
-
 Constraints:
 
 1 <= s.length <= 3 * 105
 s consist of printable ASCII characters.
-
 
 """
 
