12809d313b528fe5b6e351dbdc7e60efefad7385
HariAr2 <ha7014@srmist.edu.in>
2024-06-18 22:33:19 +0530

    added 633 leetcode MEDIUM

--- Leetcode/1431_Kids With the Greatest Number of Candies.cpp
+++ Leetcode/1431_Kids With the Greatest Number of Candies.cpp
@@ -10,7 +10,9 @@ Example 1:
 
 Input: candies = [2,3,5,1,3], extraCandies = 3
 Output: [true,true,true,false,true]
+
 Explanation: If you give all extraCandies to:
+
 - Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
 - Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
 - Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
