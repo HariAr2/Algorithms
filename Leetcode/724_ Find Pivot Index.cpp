f39b281ad0b4932cf3639cbb18576918f4f903b1
HariAr2 <ha7014@srmist.edu.in>
2024-07-08 23:10:47 +0530

    added 724 leetcode EASY

--- Leetcode/724_ Find Pivot Index.cpp
+++ Leetcode/724_ Find Pivot Index.cpp
@@ -2,20 +2,6 @@
 
 
 
-Code
-
-Testcase
-Testcase
-
-Test Result
-724. Find Pivot Index
-Solved
-Easy
-
-Topics
-Companies
-
-Hint
 Given an array of integers nums, calculate the pivot index of this array.
 
 The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
@@ -23,8 +9,6 @@ The pivot index is the index where the sum of all the numbers strictly to the le
 If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
 
 Return the leftmost pivot index. If no such index exists, return -1.
-
- 
 
 Example 1:
 
@@ -52,10 +36,7 @@ Right sum = nums[1] + nums[2] = 1 + -1 = 0
 
 */
 
-
 //solution
-
-
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
