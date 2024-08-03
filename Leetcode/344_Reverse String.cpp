8ddfc0672fe4920be3d69fbc26e577f688c41e9a
HariAr2 <ha7014@srmist.edu.in>
2024-08-03 22:54:14 +0530

    edited 344

--- Leetcode/344_Reverse String.cpp
+++ Leetcode/344_Reverse String.cpp
@@ -1,14 +1,3 @@
-class Solution {
-public:
-    void reverseString(vector<char>& s) {
-        int i = 0;
-        int j = s.size()-1;
-        while(i<j){
-            swap(s[i++],s[j--]);
-        }
-    }
-};
-
 
 
 /*
@@ -30,3 +19,14 @@ Output: ["h","a","n","n","a","H"]
 
 
 */
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i = 0;
+        int j = s.size()-1;
+        while(i<j){
+            swap(s[i++],s[j--]);
+        }
+    }
+};
